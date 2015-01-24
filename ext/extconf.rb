88420e917fe07566af06a71eaa59d49e8e0541a7
Lars Kanis <lars@greiz-reinsdorf.de>
2015-01-24 13:59:04 +0100

    Add LLVM recipe to extconf.rb and use Gallium backend for OSMesa.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,8 +3,36 @@ require 'mkmf'
 if enable_config('win32-cross')
   require "mini_portile"
 
-  LIBOSMESA_VERSION = ENV['LIBOSMESA_VERSION'] || '10.4.1'
+  LIBOSMESA_VERSION = ENV['LIBOSMESA_VERSION'] || '10.4.2'
   LIBOSMESA_SOURCE_URI = "ftp://ftp.freedesktop.org/pub/mesa/#{LIBOSMESA_VERSION}/MesaLib-#{LIBOSMESA_VERSION}.tar.bz2"
+
+  LLVM_VERSION = ENV['LLVM_VERSION'] || '3.5.1'
+  LLVM_SOURCE_URI = "http://llvm.org/releases/#{LLVM_VERSION}/llvm-#{LLVM_VERSION}.src.tar.xz"
+
+  llvmrecipe = MiniPortile.new("llvm", LLVM_VERSION)
+  llvmrecipe.files = [LLVM_SOURCE_URI]
+  llvmrecipe.target = portsdir = File.expand_path('../../ports', __FILE__)
+  # Prefer host_alias over host in order to use i586-mingw32msvc as
+  # correct compiler prefix for cross build, but use host if not set.
+  llvmrecipe.host = RbConfig::CONFIG["host_alias"].empty? ? RbConfig::CONFIG["host"] : RbConfig::CONFIG["host_alias"]
+  llvmrecipe.configure_options = [
+    "-C",
+    "--enable-optimized",
+    "--disable-assertions",
+    "--enable-targets=#{ llvmrecipe.host=~/^x86_64/ ? 'x86_64' : 'x86' }",
+    "--enable-bindings=none",
+    "--disable-libffi",
+    "--host=#{llvmrecipe.host}",
+    ]
+
+  checkpoint = File.join(portsdir, "#{llvmrecipe.name}-#{llvmrecipe.version}-#{llvmrecipe.host}.installed")
+  unless File.exist?(checkpoint)
+    ENV['LDFLAGS'] = '-static'
+    llvmrecipe.cook
+    FileUtils.touch checkpoint
+  end
+  llvmrecipe.activate
+
 
   recipe = MiniPortile.new("libosmesa", LIBOSMESA_VERSION)
   recipe.files = [LIBOSMESA_SOURCE_URI]
@@ -17,30 +45,33 @@ if enable_config('win32-cross')
     "--disable-glx",
     "--disable-dri",
     "--disable-egl",
-    "--with-dri-drivers=''",
+    "--with-dri-drivers='swrast'",
     "--with-egl-platforms=''",
-#     "--with-gallium-drivers='swrast'",
-#     "--with-llvm-shared-libs",
-#     "--enable-gallium-osmesa",
-#     "--enable-gallium-llvm=yes",
-    "--with-gallium-drivers=''",
-    "--enable-osmesa",
-    "--enable-gallium-llvm=no",
+    "--with-gallium-drivers='swrast'",
+    "--enable-gallium-osmesa",
+    "--enable-gallium-llvm",
+#     "--with-gallium-drivers=''",
+#     "--enable-osmesa",
+#     "--enable-gallium-llvm=no",
     "--enable-texture-float",
     "--enable-shared",
     "--enable-shared-glapi",
     "--target=#{recipe.host}",
     "--host=#{recipe.host}",
+    "--with-llvm-prefix=#{llvmrecipe.path}",
   ]
 
   checkpoint = File.join(portsdir, "#{recipe.name}-#{recipe.version}-#{recipe.host}.installed")
   unless File.exist?(checkpoint)
     ENV['PKG_CONFIG'] = 'not_avail'
-    ENV['CPPFLAGS'] = '-D_GLAPI_NO_EXPORTS'
+    ENV['CPPFLAGS'] = "-D_GLAPI_DLL_EXPORTS -DBUILD_GL32"
+    ENV['LDFLAGS'] = "-lws2_32 -Wl,--add-stdcall-alias"
     recipe.cook
     FileUtils.touch checkpoint
   end
   recipe.activate
+
+
   dir_config('gl', "#{recipe.path}/include", "#{recipe.path}/lib")
 
   MESA_SHARED_DLLS = %w[lib/libOSMesa-8.dll bin/libglapi-0.dll]
